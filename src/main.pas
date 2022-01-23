5e14647b76526ccbdefc08340222b7354d5d45d6
lemon4ik <lemon4ik@kali.168.42.30>
2022-01-24 01:41:12 +0300

    add select a,b,c value

--- src/main.pas
+++ src/main.pas
@@ -1,5 +1,17 @@
 program Quadratic_equations;
-
+var
+	a: integer;
+	b: integer;
+	c: integer;
+	D: integer;
 begin
+	write('Введите число a: ');
+	readln(a);
+	write('Введите число b: ');
+	readln(b);
+	write('Введите число c: ');
+	readln(c);
+
+
 
 end.
