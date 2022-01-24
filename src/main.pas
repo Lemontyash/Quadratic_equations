6340ee54a15795cbaf37d3f8d61e462ba8f36446
lemon4ik <lemon4ik@kali.168.42.30>
2022-01-24 15:58:11 +0300

    add x1 & x2 search

--- src/main.pas
+++ src/main.pas
@@ -1,9 +1,11 @@
 program Quadratic_equations;
 var
-	a: integer;
-	b: integer;
-	c: integer;
-	D: integer;
+	a:  real;
+	b:  real;
+	c:  real;
+	D:  real;
+	x1: real;
+	x2: real;
 begin
 	write('Введите число a: ');
 	readln(a);
@@ -12,6 +14,39 @@ begin
 	write('Введите число c: ');
 	readln(c);
 
+	D := (b*b) - (4 * a * c);
+	write('D = ',D:2:2);
+
+	if D > 0 then
+	begin
+		writeln('(Есть корни)');
+
+		x1 := (-b + Sqrt(D)) / (2 * a);
+		x2 := (-b - Sqrt(D)) / (2 * a);
+
+		writeln('x1: ',x1:2:2);
+		writeln('x2: ',x2:2:2);
+
+		writeln('Ответ: x1=',x1:2:2,'; x2=',x2:2:2,';');
+
+	end
+	else if D = 0 then
+	begin
+		writeln('(Один корень)');
+
+		x1 := (-b + Sqrt(D)) / (2 * a);
+		x2 := x1;
+
+		writeln('x1: ',x1:2:2);
+		writeln('x2: ',x2:2:2);
+
+		writeln('Ответ: x1= ',x1,'; x2= ',x2,';');
+
+	end
+	else
+		writeln('(Нет корней)');
+
+    
 
 
 end.
